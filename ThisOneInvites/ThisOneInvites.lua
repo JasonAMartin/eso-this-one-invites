69ff1055b193ee1a9522c0da907a33f6dce45751
devstack <devstack@CommandCenter.local>
2017-08-14 20:40:12 -0700

    feat: added auto-sending of group join requests and made a couple minor fixes.

--- ThisOneInvites/ThisOneInvites.lua
+++ ThisOneInvites/ThisOneInvites.lua
@@ -2,9 +2,10 @@
 ThisOneInvites = {}
 ThisOneInvites.name = "ThisOneInvites"
 ThisOneInvites.activatorText = ""
+ThisOneInvites.toonName = ""
 
 function ThisOneInvites:Initialize()
-  d("This One Invites 0.3 is loaded ...")
+  ThisOneInvites.toonName = GetUnitName("player"):gsub("%^.+", "")
 end
 
 function ThisOneInvites.OnAddOnLoaded(event, addonName)
@@ -20,24 +21,36 @@ function ThisOneInvites.listen(activatorText)
   if activatorText == nil or activatorText == '' then
     d("It looks like you wanted to start auto inviting. You need to specify a string as well like this: /toion x")
   else
-    d("This One Invites lisening is now turned on and listening for text: " .. activatorText)
+    d("This One Invites listening is now turned on and listening for text: " .. activatorText)
     ThisOneInvites.activatorText = activatorText
     EVENT_MANAGER:RegisterForEvent(ThisOneInvites.name, EVENT_CHAT_MESSAGE_CHANNEL, ThisOneInvites.callback)
   end
 end
 
 function ThisOneInvites.quit()
+    d(ThisOneInvites.toonName)
     d("This One Invites listening is now turned off.")
     EVENT_MANAGER:UnregisterForEvent(ThisOneInvites.name, EVENT_CHAT_MESSAGE_CHANNEL)
 end
 
+function ThisOneInvites.invitePlayer(name)
+  if name ~= ThisOneInvites.toonName then
+    GroupInviteByName(name)
+  end
+end
 
 ThisOneInvites.callback = function(_, messageType, from, message)
   name = from:gsub("%^.+", "")
   d("Checking message " .. string.lower(message) .. " from: " .. name .. " msg type " .. messageType )
+  if ThisOneInvites.activatorText == message then
+    -- send invite
+    ThisOneInvites.invitePlayer(name)
+  else
+    d("no match")
+  end
 end
 
-EVENT_MANAGER:RegisterForEvent(ThisOneInvites.name, EVENT_PLAYER_ACTIVATED, ThisOneInvites.OnAddOnLoaded)
+EVENT_MANAGER:RegisterForEvent(ThisOneInvites.name, EVENT_ADD_ON_LOADED, ThisOneInvites.OnAddOnLoaded)
 SLASH_COMMANDS["/toi"] = ThisOneInvites.help
 SLASH_COMMANDS["/toion"] = ThisOneInvites.listen
 SLASH_COMMANDS["/toioff"] = ThisOneInvites.quit
