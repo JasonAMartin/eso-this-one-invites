70533cf9ae9b1655f9b18b9d683e49bd5dc94504
devstack <devstack@CommandCenter.local>
2017-08-14 18:02:22 -0700

    feat: added slash commands, help messages and basic plumbing for chat window listening.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 An simple command-line auto-invite mod for Elder Scrolls Online.
 
-The tutorial for building this mod is here: [Let's Build a Elder Scrolls Online Mod - Part 1](/2017/08/09/ESO/build-elder-scrolls-online-mod-1/)
+The tutorial for building this mod starts here: [Let's Build a Elder Scrolls Online Mod - Part 1](/2017/08/09/ESO/build-elder-scrolls-online-mod-1/)
 
-As this point (core branch) we've laid out the basics for any ESO add-on and notified the player when the add-on is loaded.
+#### Branch Log
+
+*slash-commands*: Building on the core, I added slash commands and the ability to read the chat window.
+
+*core*: At this point I've laid out the basics for any ESO add-on and notified the player when the add-on is loaded.
--- ThisOneInvites/ThisOneInvites.lua
+++ ThisOneInvites/ThisOneInvites.lua
@@ -1,13 +1,43 @@
 -- namespace table that will hold all data and functions
 ThisOneInvites = {}
 ThisOneInvites.name = "ThisOneInvites"
+ThisOneInvites.activatorText = ""
 
 function ThisOneInvites:Initialize()
-  d("This One Invites 0.1 is loaded ...")
+  d("This One Invites 0.3 is loaded ...")
 end
 
 function ThisOneInvites.OnAddOnLoaded(event, addonName)
   ThisOneInvites:Initialize()
 end
 
+function ThisOneInvites.help()
+  d("To start auto inviting, type /toion [string] in your chat where [string] is what you want the activatorText to be. For example: /toion x")
+  d("To shut off inviting, type /toioff")
+end
+
+function ThisOneInvites.listen(activatorText)
+  if activatorText == nil or activatorText == '' then
+    d("It looks like you wanted to start auto inviting. You need to specify a string as well like this: /toion x")
+  else
+    d("This One Invites lisening is now turned on and listening for text: " .. activatorText)
+    ThisOneInvites.activatorText = activatorText
+    EVENT_MANAGER:RegisterForEvent(ThisOneInvites.name, EVENT_CHAT_MESSAGE_CHANNEL, ThisOneInvites.callback)
+  end
+end
+
+function ThisOneInvites.quit()
+    d("This One Invites listening is now turned off.")
+    EVENT_MANAGER:UnregisterForEvent(ThisOneInvites.name, EVENT_CHAT_MESSAGE_CHANNEL)
+end
+
+
+ThisOneInvites.callback = function(_, messageType, from, message)
+  name = from:gsub("%^.+", "")
+  d("Checking message " .. string.lower(message) .. " from: " .. name .. " msg type " .. messageType )
+end
+
 EVENT_MANAGER:RegisterForEvent(ThisOneInvites.name, EVENT_PLAYER_ACTIVATED, ThisOneInvites.OnAddOnLoaded)
+SLASH_COMMANDS["/toi"] = ThisOneInvites.help
+SLASH_COMMANDS["/toion"] = ThisOneInvites.listen
+SLASH_COMMANDS["/toioff"] = ThisOneInvites.quit
